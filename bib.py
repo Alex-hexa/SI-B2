18b6e15f3f8676d227798f97a2fefffa21b66ed7
Alex-hexa <alexandre.mulard@ecole-hexagone.com>
2024-05-29 09:35:39 +0200

    json

--- bib.py
+++ bib.py
@@ -1,35 +1,45 @@
+import json  # Import the json module
+import os  # Import the os module
+
 # Definition de la classe Book
 class Book:
-    def __init__(self, author, title, content):
-        self._author = author
-        self._title = title
-        self._content = content
+    def __init__(self, tag, title, image):
+        self.tag = tag
+        self.title = title
+        self.image = image
 
-# Definition de la liste library
-library = []
-
-
-# Definition des fonctions create, update, read, read-all et delete
-
+# Vérifie si le fichier 'library.json' existe
+if not os.path.exists('library.json'):
+    # Le crée s'il n'existe pas
+    with open('library.json', 'w') as file:
+        json.dump([], file)
 
 # Fonction create
 def create():
     title = input("Enter the title: ")
-    author = input("Enter the author: ")
-    content = input("Enter the content: ")
-    new_book = Book(author, title, content)
-    library.append(new_book)  # Add the new book to the library
+    tag = input("Enter the tag: ")
+    image = input("Enter the image: ")
+    new_book = Book(tag, title, image)
+    with open('library.json', 'r') as file:
+        library = json.load(file)
+    library.append(new_book.__dict__)
+    with open('library.json', 'w') as file:
+        json.dump(library, file)
     print("Book created successfully")
 
 # Fonction update
 def update():
     title = input("Enter a title: ")
+    with open('library.json', 'r') as file:
+        library = json.load(file)
     for book in library:
-        if book._title == title:
-            author = input("Enter the author: ")
-            content = input("Enter the content: ")
-            book._author = author
-            book._content = content
+        if book['title'] == title:
+            tag = input("Enter the tag: ")
+            image = input("Enter the image: ")
+            book['tag'] = tag
+            book['image'] = image
+            with open('library.json', 'w') as file:
+                json.dump(library, file)
             print("Book updated successfully")
             break
     else:
@@ -38,30 +48,38 @@ def update():
 # Fonction read
 def read():
     title = input("Enter a title: ")
+    with open('library.json', 'r') as file:
+        library = json.load(file)
     for book in library:
-        if book._title == title:
-            print("Author:", book._author)
-            print("Content:", book._content)
+        if book['title'] == title:
+            print("Tag:", book['tag'])
+            print("Image:", book['image'])
             break
     else:
         print("Book not found")
 
 # Fonction read_all
 def read_all():
+    with open('library.json', 'r') as file:
+        library = json.load(file)
+    if not library:
+        print("No books found")
     for book in library:
-        print("Title:", book._title)
-        print("Author:", book._author)
-        print("Content:", book._content)
+        print("Title:", book['title'])
+        print("Tag:", book['tag'])
+        print("Image:", book['image'])
         print("--------------------")
-    else:
-        print("No more books")
 
 # Fonction delete
 def delete():
     title = input("Enter a title: ")
+    with open('library.json', 'r') as file:
+        library = json.load(file)
     for book in library:
-        if book._title == title:
+        if book['title'] == title:
             library.remove(book)
+            with open('library.json', 'w') as file:
+                json.dump(library, file)
             print("Book deleted successfully")
             break
     else:
