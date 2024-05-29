b7c2f9d2ba4ff05d8fc0065dd0511528337bc44b
Alex-hexa <alexandre.mulard@ecole-hexagone.com>
2024-05-29 09:14:55 +0200

    add read-all

--- bib.py
+++ bib.py
@@ -1,11 +1,18 @@
+# Definition de la classe Book
 class Book:
     def __init__(self, author, title, content):
         self._author = author
         self._title = title
         self._content = content
 
+# Definition de la liste library
 library = []
 
+
+# Definition des fonctions create, update, read, read-all et delete
+
+
+# Fonction create
 def create():
     title = input("Enter the title: ")
     author = input("Enter the author: ")
@@ -14,6 +21,7 @@ def create():
     library.append(new_book)  # Add the new book to the library
     print("Book created successfully")
 
+# Fonction update
 def update():
     title = input("Enter a title: ")
     for book in library:
@@ -27,6 +35,7 @@ def update():
     else:
         print("Book not found")
 
+# Fonction read
 def read():
     title = input("Enter a title: ")
     for book in library:
@@ -37,6 +46,17 @@ def read():
     else:
         print("Book not found")
 
+# Fonction read_all
+def read_all():
+    for book in library:
+        print("Title:", book._title)
+        print("Author:", book._author)
+        print("Content:", book._content)
+        print("--------------------")
+    else:
+        print("No more books")
+
+# Fonction delete
 def delete():
     title = input("Enter a title: ")
     for book in library:
@@ -47,15 +67,18 @@ def delete():
     else:
         print("Book not found")
 
+# Programme principal
 if __name__ == "__main__":
     while True:
-        command = input("Enter a command (create, update, read, delete, exit): ").strip().lower()
+        command = input("Enter a command (create, update, read, read-all, delete, exit): ").strip().lower()
         if command == "create":
             create()
         elif command == "update":
             update()
         elif command == "read":
             read()
+        elif command == "read-all":
+            read_all()
         elif command == "delete":
             delete()
         elif command == "exit":
